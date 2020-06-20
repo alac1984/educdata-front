d93f77158caf3676d7310b77fa5900d142d5bf9c
André Carvalho <alac1984@gmail.com>
2020-06-19 23:27:48 -0300

    First commit

---
+++ build/precache-manifest.5f6aa983ee89a68a0f249b81de18480e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76f525a98d975ad1ec08b55fc0d4d36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "848c182ce6865d4c9067",
+    "url": "/static/css/2.bb926a4b.chunk.css"
+  },
+  {
+    "revision": "848c182ce6865d4c9067",
+    "url": "/static/js/2.e0f6b46a.chunk.js"
+  },
+  {
+    "revision": "5812655799cd3528cf65",
+    "url": "/static/js/main.d8ec1612.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
