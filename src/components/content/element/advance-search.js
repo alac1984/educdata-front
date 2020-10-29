0af2a9369f5727a1709911d1a93ec89151ef8005
André Carvalho <alac1984@gmail.com>
2020-10-28 21:34:52 -0300

    Some minor changes in layout

--- src/components/content/element/advance-search.js
+++ src/components/content/element/advance-search.js
@@ -1,23 +1,161 @@
-import React, { Fragment } from 'react';
-import { useSelector } from 'react-redux'
+import React, { Fragment, useState, useRef, useEffect } from 'react';
 import { cx, css } from 'emotion'
 import Typewriter from 'typewriter-effect';
-import SearchInput from './search-input'
+import { Link } from 'react-router-dom';
+import {
+   unidadesRequested,
+   userTyped,
+   userErasedAll,
+   escKeyHitted,
+   unidadeChosed
+} from '../../../Store/action/searchActions'
+import { useDispatch, useSelector } from 'react-redux'
+import { debounce } from 'lodash'
+import axios from 'axios'
+
+const ulStyle = css`
+   list-style: none;
+   background-color: white;
+   padding: 0;
+   padding-top: 10px;
+   border-left: 1px solid gray;
+   border-bottom: 1px solid gray;
+   border-right: 1px solid gray;
+`
+
+const itemStyle = css`
+   padding-left: 10px;
+   padding-right: 10px;
+   text-align: left;
+   text-transform: uppercase;
+   font-size: 14px;
+   font-weight: bold;
+   &:hover {
+      background-color: #f5f7fc;
+   }
+`
+const itemContainerStyle = css`
+   display: grid;
+   grid-template-columns: 2fr 1fr;
+   align-items: center;
+   min-height: 60px;
+   border-bottom: 1px solid #D3D3D3;
+   
+   @media (max-width: 767.98px) {
+   }
+
+   @media (max-width: 460.98px) {
+   }
+
+`
+const unidadeStyle = css`
+   margin: 0;
+   font-size: 15px;
+
+   @media (max-width: 767.98px) {
+      font-size: 15px;
+   }
+
+   @media (max-width: 460.98px) {
+      font-size: 12px;
+   }
+`
+const informationStyle = css`
+   grid-column: 2 / 2;
+   font-size: 13px;
+   list-style: none;
+   align-self: center;
+   display: flex;
+   flex-direction: column;
+   align-items: flex-end;
+
+   @media (max-width: 767.98px) {
+      font-size: 11px;
+   }
+    
+   @media (max-width: 460.98px) {
+      font-size: 9px;
+      padding: 0;
+   }
+`
+
+const idSpanStyle = css`
+   font-style: italic;
+   font-size: 12px;
+`
 
 const AdvSearch = () => {
+   const dispatch = useDispatch()
+   const unidades = useSelector(state => state.search.unidades.results)
+   const showResults = useSelector(state => state.search.showResults)
+   const isFetching = useSelector(state => state.search.isFetching)
+
+   const [query, setQuery] = useState('')
+   const [key, setKey] = useState('')
+   const debouncedUnidadesRequested = useRef(
+      debounce(
+         (query, cancelToken) => dispatch(unidadesRequested(query, cancelToken)),
+         150
+      )
+   ).current
+
+   useEffect(() => {
+      if (key !== 27) {
+         if (query !== '' || query.length >= 3) {
+            dispatch(userTyped(query))
+         }
+         const { cancel, token } = axios.CancelToken.source()
+
+         if (query !== '') {
+            debouncedUnidadesRequested(query, token)
+         } else {
+            dispatch(userErasedAll())
+         }
+         return () => cancel('Tá tudo cancelado') || debouncedUnidadesRequested.cancel()
+
+      } else {
+         dispatch(escKeyHitted())
+         document.getElementById('search-input').blur()
+      }
+
+   }, [debouncedUnidadesRequested, query, key])
 
    return (
       <Fragment>
          <div className="directory_content_area">
-            <div className="container">
+            <div className={cx("container", css`
+               height: 100%; 
+               margin-top:260px;
+
+               @media (max-width: 991px) {
+                  margin-top:100px;
+                  margin-bottom:100px;
+               }
+            `)}>
                <div className="row">
-                  <div className={cx("col-lg-10 offset-lg-1", css`
-                     padding-bottom: 120px;
-                  `)}>
+                  <div className="col-lg-10 offset-lg-1">
                      <div className="search_title_area">
                         <div>
-                           <h2 className="title mr-2">Dados educacionais para</h2>
-                           <h2 className="title ml-1 primary-color title-typewriter"><Typewriter options={{ loop: 'true' }} onInit={(typewriter) => {
+                           <h2 className={cx("title mr-2", css`
+                              @media (min-width: 576px) and (max-width: 768px) {
+                                 font-size: 2rem !important;
+                              }
+
+                              @media (min-width: 200px) and (max-width: 768px) {
+                                 font-size: 1.6rem !important;
+                              }
+                           
+                           `)}>Dados educacionais para</h2>
+                           <h2 className={cx("title ml-1 title-typewriter", css`
+                              @media (min-width: 576px) and (max-width: 768px) {
+                                 font-size: 2rem !important;
+                              }
+
+                              @media (min-width: 200px) and (max-width: 768px) {
+                                 font-size: 1.6rem !important;
+                              }
+                           
+                           `)}><Typewriter options={{ loop: 'true' }} onInit={(typewriter) => {
                               typewriter.typeString(' professores.')
                                  .pauseFor(2500)
                                  .deleteAll()
@@ -36,12 +174,97 @@ const AdvSearch = () => {
                         </div>
                      </div>{/* ends: .search_title_area */}
                      <form action="/" className="search_form">
-                        <SearchInput />
+
+                        <div className="atbd_seach_fields_wrapper">
+                           <div className="single_search_field search_query">
+                              <input className="form-control search_fields" type="text" id="search-input"
+                                 placeholder="Digite o nome de uma escola, município, estado ou região"
+                                 onChange={e => setQuery(e.target.value)}
+                                 onKeyDown={e => setKey(e.keyCode)}
+                              />
+                              {isFetching ? (
+                                 <img className={css`
+                  width: 45px;
+                  height: 45px; 
+               `} src={require('../../../img/spinner.gif')} />
+                              ) : null}
+                           </div>
+                        </div>
+
                      </form>{/* ends: .search_form */}
+                     {showResults ? (
+                        <ul className={ulStyle}>
+                           {typeof unidades === 'object' && unidades !== null ? (
+                              unidades.map((unidade, index) => (
+                                 <li className={itemStyle} key={index}>
+                                    <div className={itemContainerStyle}>
+                                       {/* Link if it is ESCOLA */}
+                                       {unidade.cd_tipo_unidade === 5 ? (
+                                          <Link className={css`grid-column: 1 / 1;`} to={'/unidade/escola/' + unidade.id_unidade}>
+                                             <div className={css`display: flex; flex-direction: column;`}>
+                                                {unidade.nm_unidade}
+                                                <span className={idSpanStyle}>ID: {unidade.id_unidade}</span>
+                                             </div>
+                                          </Link>
+                                       ) : null}
+                                       {/* Link if it is MUNICÍPIO */}
+                                       {unidade.cd_tipo_unidade === 4 ? (
+                                          <Link className={css`grid-column: 1 / 1;`} to={'/unidade/municipio/' + unidade.id_unidade}
+                                             onClick={() => dispatch(unidadeChosed(unidades[index]))}>
+                                             <div className={css`display: flex; flex-direction: column;`}>
+                                                {unidade.nm_unidade}
+                                                <span className={idSpanStyle}>ID: {unidade.id_unidade}</span>
+                                             </div>
+                                          </Link>
+                                       ) : null}
+                                       {/* Link if it is ESTADO */}
+                                       {unidade.cd_tipo_unidade === 3 ? (
+                                          <Link className={css`grid-column: 1 / 1;`} to={'/unidade/estado/' + unidade.id_unidade}>
+                                             <div className={css`display: flex; flex-direction: column;`}>
+                                                {unidade.nm_unidade}
+                                                <span className={idSpanStyle}>ID: {unidade.id_unidade}</span>
+                                             </div>
+                                          </Link>
+                                       ) : null}
+                                       {/* Link if it is REGIÃO*/}
+                                       {unidade.cd_tipo_unidade === 2 ? (
+                                          <Link className={css`grid-column: 1 / 1;`} to={'/unidade/regiao/' + unidade.id_unidade}>
+                                             <div className={css`display: flex; flex-direction: column;`}>
+                                                {unidade.nm_unidade}
+                                                <span className={idSpanStyle}>ID: {unidade.id_unidade}</span>
+                                             </div>
+                                          </Link>
+                                       ) : null}
+                                       {/* Link if it is BRASIL */}
+                                       {unidade.cd_tipo_unidade === 1 ? (
+                                          <Link className={css`grid-column: 1 / 1;`} to={'/unidade/brasil/' + unidade.id_unidade}>
+                                             <div className={css`display: flex; flex-direction: column;`}>
+                                                {unidade.nm_unidade}
+                                                <span className={idSpanStyle}>ID: {unidade.id_unidade}</span>
+                                             </div>
+                                          </Link>
+                                       ) : null}
+                                       {/* Mostrar mais resultados */}
+                                       <ul className={informationStyle}>
+                                          <li><span className='badge badge-secondary'>{unidade.nm_tipo}</span></li>
+                                          <li><span className='badge badge-primary'>{unidade.nm_unidade_pai}</span></li>
+                                       </ul>
+                                    </div>
+                                 </li>
+                              ))
+                           ) : null}
+                           <Link to={'/mais-resultados?busca=' + query}>
+                              Mostrar mais resultados
+                                 </Link>
+                        </ul>
+                     ) : null}
                   </div>{/* ends: .col-lg-10 */}
+
                </div>
+
             </div>
          </div>{/* ends: .directory_search_area */}
+
       </Fragment>
    )
 }
